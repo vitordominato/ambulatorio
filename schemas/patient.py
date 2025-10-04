430096e79b4a145d1aaecfeacef14f8bedcdb48d
vitordominato <dominato.vitor@gmail.com>
2025-10-04 13:52:28 -0300

    Update patient.py

--- schemas/patient.py
+++ schemas/patient.py
@@ -5,9 +5,23 @@ class Patient(BaseModel):
     id: str
     name: str
     sex: Literal["M","F","I"]
-    birth_date: str  # "YYYY-MM-DD"
+    age: int
     comorbidities: List[str] = []
     smoking_history_pack_years: int = 0
     is_health_worker: bool = False
+    # flags mapeadas dos fatores
+    imc_ge_25: bool = False
+    smoker_or_ex: bool = False
     is_pregnant: bool = False
+    famhx_mama: bool = False
+    famhx_prostata: bool = False
+    famhx_colorretal: bool = False
+    dm: bool = False
+    dpoc: bool = False
+    imunossuprimido: bool = False
+    cardiovascular_cronica: bool = False
+    renal_cronica: bool = False
+    hepatica_cronica: bool = False
+    neoplasia_ativa: bool = False
+
 
