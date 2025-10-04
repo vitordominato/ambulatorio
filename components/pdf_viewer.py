d9f4d923de8c517d29cda42ceb1ff91aacbe911a
vitordominato <dominato.vitor@gmail.com>
2025-10-04 12:34:55 -0300

    Update pdf_viewer.py

--- components/pdf_viewer.py
+++ components/pdf_viewer.py
@@ -1,1 +1,7 @@
+import streamlit as st
+
+def show_pdf(url: str, height: int = 720):
+    st.markdown(f"""
+        <iframe src="{url}" width="100%" height="{height}" style="border:none;"></iframe>
+    """, unsafe_allow_html=True)
 
