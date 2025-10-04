ca4aa62a19f264ee04dbcd6f8c828ffaa58d8cf5
vitordominato <dominato.vitor@gmail.com>
2025-10-04 12:36:46 -0300

    Update patient.py

--- schemas/patient.py
+++ schemas/patient.py
@@ -1,1 +1,13 @@
+from pydantic import BaseModel
+from typing import List, Literal
+
+class Patient(BaseModel):
+    id: str
+    name: str
+    sex: Literal["M","F","I"]
+    birth_date: str  # "YYYY-MM-DD"
+    comorbidities: List[str] = []
+    smoking_history_pack_years: int = 0
+    is_health_worker: bool = False
+    is_pregnant: bool = False
 
